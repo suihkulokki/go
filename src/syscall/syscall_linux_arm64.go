e1668d13de363981bd69dcc32208dfce8fb7d0c2
Riku Voipio <riku.voipio@linaro.org>
2015-12-09 15:40:18 +0200

    Add dup2 wrapper for arm64

--- src/syscall/syscall_linux_arm64.go
+++ src/syscall/syscall_linux_arm64.go
@@ -6,6 +6,10 @@ package syscall
 
 const _SYS_dup = SYS_DUP3
 
+func Dup2(oldfd int, newfd int) (err error) {
+	return Dup3(oldfd, newfd, 0)
+}
+
 //sys	Fchown(fd int, uid int, gid int) (err error)
 //sys	Fstat(fd int, stat *Stat_t) (err error)
 //sys	Fstatat(fd int, path string, stat *Stat_t, flags int) (err error)
